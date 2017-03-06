f0357a3b465e8d561f8a3b997fd8cc03802c6e1b
Miguel Torres <mta1830@gmail.com>
2017-03-06 05:42:41 -0400

    Installing bootstrap 3 at the project

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <h1>Hola, mundo!</h1>
+    <p>Este es mi proyecto de Laravel Eloquent ORM.</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Aprender más &raquo;</a></p>
+    <hr>
+@endsection
